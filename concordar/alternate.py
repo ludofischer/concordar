62cab4631653a58a59f449f956e13e9dffae9b2c
Ludovico Fischer <livrerie@gmail.com>
2010-09-30 15:34:25 +0200

    Renamed some methods.

--- concordar/alternate.py
+++ concordar/alternate.py
@@ -4,17 +4,20 @@ from PyQt4 import QtGui
 from concordance import lowercase_extractor, numerize
 
 def search_sequence(sequence, word, width):
-   for coord, match in get_word_groups(sequence, build_ranges(positions(sequence, word), width, len(sequence))):
+   for result in build_results(get_word_groups(sequence, symmetric_ranges(positions(sequence, word), width, len(sequence)))):
+      yield result
+      
+def build_results(sequence):
+   for coord, match in sequence:
       words = [item[1] for item in match]
-      yield(coord, ' '.join(words))
-     
+      yield (coord, ' '.join(words))
 
 def get_word_groups(sequence, ranges):
     from itertools import islice
     for coord, start, end in ranges:
         yield (coord, sequence[start:end])
 
-def build_ranges(iterable, width, maximum):
+def symmetric_ranges(iterable, width, maximum):
     for index, coord in iterable:
         if index - width < 0:
             start = 0
