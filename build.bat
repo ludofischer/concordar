101f9341a80dc5dd062a042b68e00fdb2b0d0350
Ludovico Fischer <livrerie@gmail.com>
2010-10-01 18:38:53 +0200

    Reduce reference to version number.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 "c:\Program Files\Windows Installer XML v3.5\bin\candle.exe" package.xml
-"c:\Program Files\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension -out "Install Concordar 0.1.2.msi" package.wixobj
+"c:\Program Files\Windows Installer XML v3.5\bin\light.exe" -ext WixUIExtension -out "Install Concordar.msi" package.wixobj
