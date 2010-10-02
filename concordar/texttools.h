de2ea74de6f9baf881e7e6fe21d922b0460ba583
Ludovico Fischer <livrerie@gmail.com>
2010-10-03 00:18:48 +0200

    Updated textools.h.

--- concordar/texttools.h
+++ concordar/texttools.h
@@ -1,6 +1,25 @@
-#include "ui_main_window.h"
+#include <QWidget>
 
-class TextTools : public QMainWindow, public Ui::Ui_MainWindow
-{
-  
+namespace Ui {
 }
+class TextTools : public QWidget {
+  
+  Q_OBJECT
+
+  public:
+  TextTools(QWidget *parent = 0);
+  void change_working_file(filename);
+                                    
+public slots:
+  void move_cursor_to_word(const QModelIndex&);
+  void update_concordance();
+                           
+                                              
+private slots:
+  void choose_file();
+  void new_word_selected_in_text();
+  void new_word_typed(const Qstring&);
+  
+private:
+  QString Word;
+};
