616c6b93c103d21800ff6c098b443bc35ef1ce19
Ludovico Fischer <livrerie@gmail.com>
2010-10-03 12:09:09 +0200

    Update setup.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 setup(
-    name='concordance',
+    name='concordar',
     version='0.1alpha',
     description='A simple GUI tool for building concordances',
     long_description='A simple GUI tool for building concordances',
     license='GNU GPL v3',
     author='Ludovico Fischer',
     author_email='livrerie@gmail.com',
-    url='http://github.com/ludovicofischer/Concordance',
-    package_dir={'': 'concordance'},
-    py_modules=['concordance', 'texttools','ui_main_window'],
-    scripts=['concordance/main.py'],
-    requires=['PyQt',]
+    url='http://github.com/ludovicofischer/concordar',
+    package_dir={'': 'concordar'},
+    py_modules=['concordance', 'texttools','ui_main_window', 'models'],
+    scripts=['concordar.py'],
+    requires=['PyQt4',]
 )
     
     
