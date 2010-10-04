8ea0f89e39dd1fcb2c8c6cd73b6c86b5af003ecc
Ludovico Fischer <livrerie@gmail.com>
2010-10-05 01:38:36 +0200

    Added docstrings.

--- concordar/concordance.py
+++ concordar/concordance.py
@@ -4,6 +4,7 @@ from PyQt4 import QtGui
 
 
 def lowercase_extractor(word):
+"""Returns a function returning the index of any matching token."""
     def configured_extractor(index, match):
         if match.lower() == word.lower():
             return index
@@ -11,15 +12,18 @@ def lowercase_extractor(word):
     return configured_extractor
 
 def search_sequence(sequence, word, width):
+"""Yields matching strings and their visual positioning."""
    for result in build_results(sequence, symmetric_ranges(positions(sequence, word), width, len(sequence))):
       yield result
       
 def build_results(sequence, ranges):
+"""Yields visual position and corresponding string for ranges in sequence."""
    for index, start, end in ranges:
       words = [item[2] for item in sequence[start:end]]
       yield (sequence[index][1], ' '.join(words))
 
 def symmetric_ranges(iterable, width, maximum):
+"""Returns ranges of the specified width for iterable."""
     for index in iterable:
         if index - width < 0:
             start = 0
@@ -33,6 +37,7 @@ def symmetric_ranges(iterable, width, maximum):
         yield (index, start, end)
 
 def positions(sequence, word, criterion_definition=lowercase_extractor):
+"""Returns the indices of the matching tokens."""
     matches_criterion = criterion_definition(word)
     return filter(lambda x: x is not None, [matches_criterion(index, thing) for (index, coord, thing) in sequence])
        
